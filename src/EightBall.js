6fd65de967d76b1876b89554ea9e188e6aef055f
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-10-24 17:54:35 -0700

    further study 1

--- src/EightBall.js
+++ src/EightBall.js
@@ -7,8 +7,11 @@ import GetRandom from './GetRandom'
 
 let EightBall = () => {
 
-  const [msg, setMsg] = useState("Think of a Question.");
-  const [color, setColor] = useState("black");
+  const initialMsg =  "Think of a Question.";
+  const initialColor = "black"
+
+  const [msg, setMsg] = useState(initialMsg);
+  const [color, setColor] = useState(initialColor);
 
 
   let shake = () => {
@@ -17,20 +20,25 @@ let EightBall = () => {
     setColor(color)
   };
 
+  let reset = () => {
+    setMsg(initialMsg)
+    setColor(initialColor)
+  }
+
     return (
       <section className="stage">
-        <figure
-          className="ball"
-          style={{ backgroundColor: color }}
-        >
+        <figure className="ball" style={{ backgroundColor: color }}>
           <span className="shadow"></span>
           <span className="text">
             <h1 style={{ color: color }}>{msg}</h1>
-            <button onClick={() => shake()}>
+            <button className="shake" onClick={() => shake()}>
               Shake
             </button>
           </span>
         </figure>
+        <button className="reset" onClick={() => reset()}>
+          Reset
+        </button>
       </section>
     );
 
